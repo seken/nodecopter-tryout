94c47190e6b2be90c7ca782520eafc91253eaee3
Rob Griffiths <rob@bytespider.eu>
2013-03-23 22:34:43 +0000

    semi working stalker copter

--- png-stream.js
+++ png-stream.js
@@ -1,12 +1,11 @@
 // Run this to receive a png image stream from your drone.
 
-var arDrone = require('..');
+var arDrone = require('ar-drone');
 var http    = require('http');
 var fs = require('fs');
 
-console.log('Connecting png stream ...');
-
-var pngStream = arDrone.createPngStream();
+//console.log('Connecting png stream ...');
+//var pngStream = arDrone.createPngStream();
 BOTTOM=3
 TOP=0
 client = arDrone.createClient()
@@ -15,6 +14,8 @@ client.config('general:navdata_demo', 'FALSE');
 client.config('detect:enemy_colors', '3');
 client.config('detect:detect_type', '10');
 client.config('detect:detections_select_h', '1');
+client.config('control:altitude_max', '2000');
+
 navdata = arDrone.createUdpNavdataStream()
 
 
@@ -31,6 +32,7 @@ var s = net.createServer(function(c) {
 })
 s.listen(3002);
 
+/*
 var lastPng;
 var i = 0;
 pngStream
@@ -54,34 +56,45 @@ var server = http.createServer(function(req, res) {
 server.listen(8080, function() {
   console.log('Serving latest png on port 8080 ...');
 });
+*/
 
 client.stop();
 client.takeoff();
+
+client.up(0.5);
+client.after(1000, function () {
+	client.stop();
+});
+
+var found = false;
+
 client.after(2000, function() {
 	client.on('navdata', function(data) {
 		try {
-		//		console.log(data)
-		    console.log('Battery: ' + data.demo.batteryPercentage)
-		//    console.log('Alt: ' + data.demo.altitude)
-		//	console.log(data.visionDetect);
+			if (data.demo.batteryPercentage < 20) {
+				console.log('Battery: ' + data.demo.batteryPercentage);
+			}
+
+		    //console.log('Alt: ' + data.demo.altitude);
+		    //console.log(data.visionDetect);
 
 			if (data.visionDetect.nbDetected > 0) {
+
+				client.stop();
+
 				vd = data.visionDetect
-				if (vd.dist[0] > 60) {
+				console.log('distance', vd.dist);
+
+				if (vd.dist[0] > 100) {
 					console.log('forwards' + vd.dist[0])
-					client.stop();
-					client.front(0.1);
-					//client.stop();
-				} else if (vd.dist[0] < 50)  {
+					client.front(0.2);
+				} else if (vd.dist[0] < 100)  {
 					console.log('back')
-					client.stop()
-					client.back(0.1);
+					client.back(0.2);
 				} else {
-					console.log('@')
-		//			client.front(0);
-					client.stop()
+					console.log('nothing to do...')
 				}
-	/*
+
 				if (vd.yc > 550) {
 					client.down(0.2);
 				} else if (vd.yc < 450) {
@@ -97,17 +110,20 @@ client.after(2000, function() {
 				} else {
 					client.left(0);
 				}
-	*/
+				
 			} else {
+				// look around for one
 				console.log('none found')
-				client.stop()
-				//client.up(0);
-				//client.front(0);
-				client.clockwise(0.1);
+				client.clockwise(.7);
 			}
 		} catch (error) {}
 
 	});
 });
-
-
+process.on('SIGINT', function () {
+	client && client.land();
+	setTimeout(function () {
+		process.exit(0);
+	}, 1000)
+});
+//client.land();
